4cf8430c2da495150f0eb9ff5cb5f62d4d868a16
Branko <jankovicb1003@gmail.com>
2026-08-19 23:22:37 +0200

    finalni model

--- src/model_final.py
+++ src/model_final.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
  
 from data_preprocessing import (
     split_features_and_target,
@@ -11,7 +12,6 @@ from data_preprocessing import (
 )
 
 DATA_PATH = "data/cars_cleaned_with_features.csv"
-MODEL_PATH = "models/car_price_final_model.joblib"
 
 #ucitavanje skupa podataka
 print("Loading dataset...")
@@ -43,14 +43,19 @@ model = Pipeline(
     ]
 )
 
-#treniranje modela
-print("Training model...")
- 
 model.fit(X_train, y_train)
+ 
+y_pred = model.predict(X_test)
+ 
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = mse ** 0.5
+r2 = r2_score(y_test, y_pred)
 
-#cuvanje modela
-print("Saving model...")
+metrics = pd.DataFrame({
+    "metric": ["MAE", "MSE", "RMSE", "R2"],
+    "value": [mae, mse, rmse, r2],
+})
  
-joblib.dump(model, MODEL_PATH)
- 
-print(f"Model saved to: {MODEL_PATH}")
+print("\nRandom Forest:")
+print(metrics)
